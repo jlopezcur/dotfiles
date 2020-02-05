c52958d8dc7ebad50809bc52dc4d232594e37a97
Javier López Úbeda <jlopezcur@gmail.com>
2020-02-05 02:12:07 +0100

    Added some alias and cargo to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,7 @@ set theme_color_scheme gruvbox
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 
+set PATH ~/.cargo/bin $PATH
+alias ll "br -dp"
+alias ls "br --sizes"
+set EDITOR "nvim"
