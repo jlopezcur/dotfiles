8c46ce5e6f6de9a87980ed86a34ab46ba6cce13a
Javier López Úbeda <jlopezcur@gmail.com>
2020-02-08 02:41:44 +0100

    Added some alias to fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 
 set PATH ~/.cargo/bin $PATH
-alias ll "br -dp"
-alias ls "br --sizes"
-set EDITOR "nvim"
+
+alias lb "br -dp"
+alias lv "br --sizes"
+
+alias update "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
