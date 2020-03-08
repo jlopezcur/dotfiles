a33f54cc3476e3a08ab592021cefe8d93e9d72d2
Javier López Úbeda <jlopezcur@gmail.com>
2020-03-08 23:29:55 +0100

    Updated fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,11 @@ set theme_color_scheme gruvbox
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 
-set PATH ~/.cargo/bin $PATH
-set PATH ~/bin $PATH
-set PATH ~/.npm-global/bin $PATH
+set PATH ~/.cargo/bin:$PATH
+set PATH ~/bin:$PATH
+set PATH ~/.npm-global/bin:$PATH
 
 alias lb "br -dp"
 alias lv "br --sizes"
 
-alias update "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
+alias update "sudo apt update && sudo apt upgrade && sudo apt autoremove -y"
