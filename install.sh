a7c97483bfad6c39aa256efddfbc6cba72c174a0
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-19 01:32:44 +0200

    Added spotify

--- install.sh
+++ install.sh
@@ -108,6 +108,12 @@ echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable
 sudo apt update
 sudo apt install -y brave-browser
 
+# Spotify
+
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update && sudo apt install -y spotify-client
+
 # Office Tools
 
 echo "Installing Office Tools..."
