8f9c0ca32bdfe6e54bdf4f3dca004007b496d69e
Javier López Úbeda <jlopezcur@gmail.com>
2020-10-16 09:24:34 +0200

    feat: added tree sitter incremental selection

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -4,4 +4,13 @@ require'nvim-treesitter.configs'.setup {
 		enable = true,
 		disable = { },
 	},
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm"
+    },
+  },
 }
