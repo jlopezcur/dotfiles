e7700715b2d3c328cfe97e54d96a69ff9ee88897
Javier López Úbeda <jlopezcur@gmail.com>
2020-10-24 03:44:34 +0200

    feat(xmonad): adjust the font size

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -74,7 +74,7 @@ import XMonad.Prompt.ConfirmPrompt
 ------------------------------------------------------------------------
 
 myFont :: String
-myFont = "xft:SauceCodePro Nerd Font Mono:regular:size=11:antialias=true:hinting=true"
+myFont = "xft:SauceCodePro Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
 
 myModMask :: KeyMask
 myModMask = mod4Mask -- Sets modkey to super/windows key
