e0f7ed71e5478d664d8cb0aeb4d28add2e0c3d14
Javier López Úbeda <jlopezcur@gmail.com>
2020-11-05 22:07:36 +0100

    feat(fish): added bang bang functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -44,3 +44,28 @@ end
 
 [ -f ~/.config/fish/aliases.fish ] && . ~/.config/fish/aliases.fish
 [ -f ~/.config/fish/config-private.fish ] && . ~/.config/fish/config-private.fish
+
+# BangBang
+# https://github.com/oh-my-fish/plugin-bang-bang/
+
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
