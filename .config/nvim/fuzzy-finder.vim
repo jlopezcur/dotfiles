bf17c51425406f5d68d581d7a30dd2c194a561e8
Javier López Úbeda <jlopezcur@gmail.com>
2020-12-21 03:14:22 +0100

    feat(nvim): replace gr with telescope one
    - also added several telescope pickers

--- .config/nvim/fuzzy-finder.vim
+++ .config/nvim/fuzzy-finder.vim
@@ -2,3 +2,7 @@ nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+
+nnoremap gr <cmd>Telescope lsp_references<cr>
+nnoremap <leader>fB <cmd>Telescope git_branches<cr>
+nnoremap <leader>gS <cmd>Telescope git_status<cr>
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -7,7 +7,6 @@ local on_attach_vim = function(client)
 
   vim.api.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
   vim.api.nvim_buf_set_keymap(0, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap = true})
 
   vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
   vim.api.nvim_buf_set_keymap(0, 'i', '<c-K>', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
