a14dcf150fad8469bc8152e93cab5f9d42a6f608
Javier López Úbeda <jlopezcur@gmail.com>
2020-12-24 05:13:58 +0100

    chore(xmonad): removed space

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -367,7 +367,7 @@ main = do
   xmonad $ customKeys $ ewmh desktopConfig
     { manageHook = myManageHook <+> manageSpawn <+> manageHook desktopConfig <+> manageDocks
     , logHook = dynamicLogWithPP xmobarPP
-      { ppOutput = \x -> hPutStrLn xmproc0 x  >> hPutStrLn xmproc1 x
+      { ppOutput = \x -> hPutStrLn xmproc0 x >> hPutStrLn xmproc1 x
       , ppCurrent = xmobarColor "grey" "" . wrap "[" "]" -- Current workspace in xmobar
       , ppVisible = xmobarColor "grey" ""                -- Visible but not current workspace
       , ppHidden = xmobarColor "grey" "" . wrap "*" ""   -- Hidden workspaces in xmobar
