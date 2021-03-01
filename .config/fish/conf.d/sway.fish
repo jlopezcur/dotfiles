5df4cc5b915890ff45dd4effad3d0641fe32fada
Javier López Úbeda <jlopezcur@gmail.com>
2021-03-01 22:31:52 +0100

    feat: added Sway environment variables

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,5 +1,11 @@
 # If running from tty1 start sway
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+  # Your environment variables
+  export QT_QPA_PLATFORM=wayland
+  export MOZ_ENABLE_WAYLAND=1
+  export MOZ_WEBRENDER=1
+  export XDG_SESSION_TYPE=wayland
+  export XDG_CURRENT_DESKTOP=sway
   exec sway
 end
