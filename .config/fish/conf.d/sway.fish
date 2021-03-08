a61d8384161cba741948380cb8818608d923fe0c
Javier López Úbeda <jlopezcur@gmail.com>
2021-03-08 01:33:02 +0100

    feat(fish): commented pipewire debug

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,11 +1,11 @@
 # If running from tty1 start sway
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
-  # Your environment variables
   export QT_QPA_PLATFORM=wayland
   export MOZ_ENABLE_WAYLAND=1
   export MOZ_WEBRENDER=1
   export XDG_SESSION_TYPE=wayland
   export XDG_CURRENT_DESKTOP=sway
+  # export PIPEWIRE_DEBUG=4
   exec sway
 end
