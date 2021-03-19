86afb7226f1f3c1b2e57df2992f6638e76fafc9b
Javier López Úbeda <jlopezcur@gmail.com>
2021-03-19 17:45:10 +0100

    feat(sway): remove log verbose mode

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -8,5 +8,5 @@ if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   export XDG_CURRENT_DESKTOP=sway
   export FONTCONFIG_PATH=/etc/fonts
   # export PIPEWIRE_DEBUG=4
-  exec sway -V > /home/jlopez/.cache/sway.log 2>&1
+  exec sway > /home/jlopez/.cache/sway.log 2>&1
 end
