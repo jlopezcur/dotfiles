ea450ab54b2340e556ba66a85d22916ab3bb0fd3
Javier López Úbeda <jlopezcur@gmail.com>
2021-03-20 18:40:52 +0100

    fix(waybar): check updates modules

--- .config/waybar/modules/counter.sh
+++ .config/waybar/modules/counter.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-list="$(cat -)"
-count=$(echo "$list" | wc -l)
-plainlist="$(echo "$list" | sed ':a; N; $!ba; s/\n/\\n/g')"
+list=$(cat)
+
+if [[ $list = "" ]]; then
+  printf '{"text":"", "class":"none", "alt":"none", "tooltip":""}\n'
+  exit
+fi
+
+count=$(echo $list | wc -l)
+plainlist="$(echo $list | sed ':a; N; $!ba; s/\n/\\n/g')"
 
 if [[ "$count" -eq 0 ]]; then
   class="none"
