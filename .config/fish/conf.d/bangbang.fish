ab6ef58f727eccf3d9a753c3b3666929b8b188a8
Javier López Úbeda <jlopezcur@gmail.com>
2021-03-28 18:24:02 +0200

    feat(fish): extract bangbang behavior

---
+++ .config/fish/conf.d/bangbang.fish
@@ -0,0 +1,25 @@
+# BangBang
+# https://github.com/oh-my-fish/plugin-bang-bang/
+
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
+
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,31 +33,6 @@ set ARCHFLAGS "-arch x86_64"
 
 [ -f ~/.config/fish/aliases.fish ] && . ~/.config/fish/aliases.fish
 
-# BangBang
-# https://github.com/oh-my-fish/plugin-bang-bang/
-
-function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
-end
-
-function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
-end
-
-bind ! __history_previous_command
-bind '$' __history_previous_command_arguments
-
 # Starship
 
 starship init fish | source
