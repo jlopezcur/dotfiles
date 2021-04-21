bdc6ff1f21427d93b6b4eb8a43fca511a51e8652
Javier López Úbeda <jlopezcur@gmail.com>
2021-04-21 19:23:35 +0200

    feat(nvim): added more features to completion

--- .config/nvim/lua/lt/lsp/completion.lua
+++ .config/nvim/lua/lt/lsp/completion.lua
@@ -2,9 +2,17 @@ vim.o.completeopt = "menuone,noselect"
 
 require'compe'.setup {
   enabled = true,
+  autocomplete = true,
   debug = false,
-  min_length = 2,
+  min_length = 1,
   preselect = 'enable',
+  throttle_time = 80,
+  source_timeout = 200,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = true,
   -- preselect = 'enable' || 'disable' || 'always';
   -- throttle_time = ... number ...;
   -- source_timeout = ... number ...;
