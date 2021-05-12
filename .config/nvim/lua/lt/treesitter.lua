538c0815529ab334e5f7f3bf38c9199e6636e1b6
Javier López Úbeda <jlopezcur@gmail.com>
2021-05-13 01:21:03 +0200

    feat(nvim): treesitter spellcheck only in comments

--- .config/nvim/lua/lt/treesitter.lua
+++ .config/nvim/lua/lt/treesitter.lua
@@ -1,6 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained",
-  highlight = { enable = true },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true
+  },
   incremental_selection = {
     enable = true,
     keymaps = {
