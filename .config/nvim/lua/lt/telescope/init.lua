a44cf014ef7227622278873efcf9fbde8e63d9d4
Javier López Úbeda <jlopezcur@gmail.com>
2021-05-21 02:37:14 +0200

    feat(nvim): moved some telescope bindings

--- .config/nvim/lua/lt/telescope/init.lua
+++ .config/nvim/lua/lt/telescope/init.lua
@@ -9,13 +9,12 @@ require('telescope').setup {
 options = { noremap = true }
 map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>", options)
 map('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<CR>", options)
-map('n', '<leader>b', "<cmd>lua require('telescope.builtin').buffers()<CR>", options)
-map('n', '<leader>r', "<cmd>lua require('telescope.builtin').registers()<CR>", options)
+map('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>", options)
+map('n', '<leader>fr', "<cmd>lua require('telescope.builtin').registers()<CR>", options)
 map('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<CR>", options)
 
 map('n', 'gr', "<cmd>lua require('telescope.builtin').lsp_references()<CR>", options)
 map('n', '<leader>gb', "<cmd>lua require('telescope.builtin').git_branches()<CR>", options)
 map('n', '<leader>gc', "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", options)
 -- map('n', '<leader>gs', "<cmd>lua require('telescope.builtin').git_status()<CR>", options)
-map('n', '<leader>rc', "<cmd>lua require('telescope.builtin').find_files({ cwd = '~/dev/dotfiles/.config/'})<CR>", options)
-map('n', '<leader>d', "<cmd>lua require('telescope.builtin').find_files({ cwd = '~/dev/', file_ignore_patterns = { 'node_modules' } })<CR>", options)
+-- map('n', '<leader>rc', "<cmd>lua require('telescope.builtin').find_files({ cwd = '~/dev/dotfiles/.config/'})<CR>", options)
