404f13cce6958364ede796022f1d789f332df57b
Javier López Úbeda <jlopezcur@gmail.com>
2021-06-23 23:10:53 +0200

    feat(fish): added more wayland global variables

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -2,6 +2,9 @@
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   export QT_QPA_PLATFORM=wayland
+  export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+  export SDL_VIDEODRIVER=wayland
+  export _JAVA_AWT_WM_NONREPARENTING=1
   export MOZ_ENABLE_WAYLAND=1
   export MOZ_WEBRENDER=1
   export XDG_SESSION_TYPE=wayland
