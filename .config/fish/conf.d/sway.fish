4ffb435c5e022d33180d655119289d5583b5c0c6
Javier López Úbeda <jlopezcur@gmail.com>
2021-09-06 12:33:59 +0200

    fix(sway): steam launch problems

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -3,11 +3,11 @@ set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   export QT_QPA_PLATFORM=wayland
   export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-  export SDL_VIDEODRIVER=wayland
+  # export SDL_VIDEODRIVER=wayland
   export _JAVA_AWT_WM_NONREPARENTING=1
   export MOZ_ENABLE_WAYLAND=1
   export MOZ_WEBRENDER=1
-  export XDG_SESSION_TYPE=wayland
+  # export XDG_SESSION_TYPE=wayland
   export XDG_CURRENT_DESKTOP=sway
   export FONTCONFIG_PATH=/etc/fonts
   # export PIPEWIRE_DEBUG=4
