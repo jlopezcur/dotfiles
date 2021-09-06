b05e7e8f2c7d08799e09073eeff3c064f54400f5
Javier López Úbeda <jlopezcur@gmail.com>
2021-09-06 12:30:03 +0200

    feat(waybar): added mouse and keyboard battery

--- .config/waybar/modules/keyboard_battery.sh
+++ .config/waybar/modules/keyboard_battery.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-pct=$(upower -i /org/freedesktop/UPower/devices/keyboard_hid_dco2co26oeeo2do69_battery | grep percentage | awk '{print $2}')
+check=$(upower -i /org/freedesktop/UPower/devices/keyboard_hid_dco2co26oeeo2do69_battery | rg battery-level | awk '{print $2}')
+
+if [[ $check = "unknown" ]]; then
+  pct="--"
+else
+  pct=$(upower -i /org/freedesktop/UPower/devices/keyboard_hid_dco2co26oeeo2do69_battery | rg percentage | awk '{print $2}')
+fi
 
 printf '{"text":"%s", "class":"one", "alt":"one", "tooltip":"%s"}\n' \
     "$pct" "Keyboard battery: $pct" | jq --unbuffered --compact-output
