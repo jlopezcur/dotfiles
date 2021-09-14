e25a1fcf01ee1390656e045a0582c2a5b7732604
Javier López Úbeda <jlopezcur@gmail.com>
2021-09-14 03:50:50 +0200

    feat(scrcpy): moved to stow

--- setup.sh
+++ setup.sh
@@ -7,28 +7,23 @@ echo "Installing from $DIR..."
 stow -vt ~ fish # Fish
 stow -vt ~ starship # Starship
 stow -vt ~ git # Git
-stow -vt ~ sway # Window Manager
+stow -vt ~ sway # Window manager
 stow -vt ~ waybar # System bar
 # stow -vt ~ workstyle
 stow -vt ~ mako # Notifications
 stow -vt ~ swaylock # Locker
 stow -vt ~ lf # File manager
-stow -vt ~ alacritty # Terminal Emulator
+stow -vt ~ alacritty # Terminal emulator
 stow -vt ~ nvim # Editor
-stow -vt ~ qutebrowser # Primary Browsers
-stow -vt ~ brave # Secondary Browser
+stow -vt ~ qutebrowser # Primary browsers
+stow -vt ~ brave # Secondary browser
 stow -vt ~ newsboat # Newsboat
 stow -vt ~ zathura # PDF Reader
 stow -vt ~ slack # Slack
+stow -vt ~ scrcpy # Mobile viewer
 
 # Scripts
 ln -sf "$DIR/.local/bin/create-gif" ~/.local/bin/create-gif
 ln -sf "$DIR/.local/bin/create-video" ~/.local/bin/create-video
 
-# App launchers
-mkdir -p ~/.local/share/applications
-ln -sf "$DIR/.local/share/applications/scrcpy.desktop" ~/.local/share/applications/scrcpy.desktop
-
 echo "DONE!"
-
-
