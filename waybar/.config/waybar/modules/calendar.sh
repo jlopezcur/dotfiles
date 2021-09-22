1cebcc8f436084383c27998aa355bc361801cd4e
Javier López Úbeda <jlopezcur@gmail.com>
2021-09-23 00:10:23 +0200

    feat(waybar): added some items
    - scratchpad counter
    - weather
    - better vpn from doc

--- waybar/.config/waybar/modules/calendar.sh
+++ waybar/.config/waybar/modules/calendar.sh
@@ -2,14 +2,8 @@
 
 text=$(date +'%a, %d/%m/%Y')
 
-list=$(calcurse -Q)
-plainlist="$(echo "$list" | sed ':a; N; $!ba; s/\n/\\n/g')"
+events="$(calcurse -Q --output-datefmt %d/%m/%Y | sed ':a; N; $!ba; s/\n/\r/g')"
+calendar="$(cal -1 --color=always | sed ':a; N; $!ba; s/\n/\r/g')"
+tooltip=$(echo "$calendar\n\n$events")
 
-calendar=$(cal -3 --color=never)
-plaincalendar="$(echo "$calendar" | sed ':a; N; $!ba; s/\n/\\n/g')"
-
-tooltip=$(echo "$plaincalendar\n$plainlist")
-
-printf '{"text":"%s", "class":"one", "alt":"one", "tooltip":"%s"}\n' \
-    "$text" "$tooltip" | jq --unbuffered --compact-output
-
+echo '{ "text": "'$text'", "class": "calendar", "tooltip": "'$tooltip'"}'
