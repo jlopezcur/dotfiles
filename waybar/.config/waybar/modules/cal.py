edeb00e8abacc10800b22671619a0c0ac6469a46
Javier López Úbeda <jlopezcur@gmail.com>
2021-09-27 18:55:45 +0200

    feat(waybar): moved calendar to cal
    - avoid python conflict

---
+++ waybar/.config/waybar/modules/cal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import json
+import subprocess
+from datetime import datetime
+
+calendar = subprocess.check_output("cal -3 --color=always", shell=True, universal_newlines=True)
+events = subprocess.check_output("calcurse -Q --output-datefmt %d/%m/%Y", shell=True, universal_newlines=True)
+text = datetime.now().strftime("%a, %d/%m/%Y")
+
+calendar = calendar.replace("\u001b[7m", '<b><u>').replace("\u001b[27m", "</u></b>")
+
+data = {}
+data['text'] = text
+data['class'] = "calendar"
+data['tooltip'] = calendar + "\n" + events
+
+print(json.dumps(data))
--- waybar/.config/waybar/modules/calendar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-text=$(date +'%a, %d/%m/%Y')
-
-events="$(calcurse -Q --output-datefmt %d/%m/%Y | sed ':a; N; $!ba; s/\n/\r/g')"
-calendar="$(cal -1 --color=always | sed ':a; N; $!ba; s/\n/\r/g')"
-tooltip=$(echo "$calendar\n\n$events")
-
-echo '{ "text": "'$text'", "class": "calendar", "tooltip": "'$tooltip'"}'
