c835ba3416f902770facf34ae9a203ee2f930e94
Javier López Úbeda <jlopezcur@gmail.com>
2021-10-09 16:56:03 +0200

    feat(qute): change youtube-dl for yt-dlp

--- qutebrowser/.config/qutebrowser/mappings.py
+++ qutebrowser/.config/qutebrowser/mappings.py
@@ -2,10 +2,10 @@
 config.bind(',om', 'hint links spawn mpv {hint-url}')
 
 # Bind for download a youtube video with youtube-dl
-config.bind(',dv', 'hint links spawn alacritty --class downloader -e youtube-dl -o ~/Downloads/%(title)s.%(ext)s {hint-url}')
+config.bind(',dv', 'hint links spawn alacritty --class downloader -e yt-dlp -o ~/Downloads/%(title)s.%(ext)s {hint-url}')
 
 # Bind for download a youtube audio with youtube-dl
-config.bind(',ds', 'hint links spawn alacritty --class downloader -e youtube-dl -i --extract-audio --audio-format mp3 --audio-quality 0 -o ~/music/%(title)s.%(ext)s {hint-url}')
+config.bind(',ds', 'hint links spawn alacritty --class downloader -e yt-dlp -i --extract-audio --audio-format mp3 --audio-quality 0 -o ~/music/%(title)s.%(ext)s {hint-url}')
 
 config.bind(',xb', 'config-cycle statusbar.show always never')
 config.bind(',xt', 'config-cycle tabs.show always never')
