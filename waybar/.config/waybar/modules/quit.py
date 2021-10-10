b2cc4b4ace23d691959f31ddfa8ceb15590a6644
Javier López Úbeda <jlopezcur@gmail.com>
2021-10-11 00:00:56 +0200

    feat(waybar): improved quit module

--- waybar/.config/waybar/modules/quit.py
+++ waybar/.config/waybar/modules/quit.py
@@ -15,8 +15,16 @@ day_in_seconds = (24 * 60 * 60)
 total = (per_day * elapsed_seconds) / day_in_seconds
 total_cost = (cost_per_day * elapsed_seconds) / day_in_seconds
 
+tooltip_lines = [
+    "Time: " + str(delta.days) + " days",
+    "Units: " + str(int(round(total, 0))),
+    "Money: " + str(round(total_cost, 2)) + "€",
+]
+
+
 data = {}
-data['text'] = str(int(round(total, 0))) + " " + emoji + " " + str(round(total_cost, 2)) + "€"
+data['text'] = emoji + " " + str(int(round(total, 0)))
 data['class'] = "quit"
+data['tooltip'] = "\n".join(tooltip_lines)
 
 print(json.dumps(data))
