c259213d75baa2ac39bb26144da7666f448b8bdc
Javier López Úbeda <jlopezcur@gmail.com>
2021-10-10 16:39:33 +0200

    feat(sway): added MANPAGER to man command

--- sway/.config/sway/scripts/man.sh
+++ sway/.config/sway/scripts/man.sh
@@ -3,4 +3,5 @@
 # Requirements: bemenu
 
 page="$(apropos . | awk '{print $1}' | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p 'Man Page')"
-[ -z "$page" ] || alacritty -e man $page
+export MANPAGER="nvim -c 'set ft=man' -"
+[ -n "$page" ] && alacritty -e man $page
