dd52534e894cea9366e0993c0a7db832b87b1ff8
Javier López Úbeda <jlopezcur@gmail.com>
2021-10-10 16:39:03 +0200

    feat(sway): scripts with positive condition

--- sway/.config/sway/scripts/password.sh
+++ sway/.config/sway/scripts/password.sh
@@ -3,4 +3,4 @@
 # Requirements: gopass & bemenu
 
 key="$(gopass ls -f | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p 'Password')"
-[ -z ${key} ] || gopass -o -c $key
+[ -n ${key} ] && gopass -o -c $key
