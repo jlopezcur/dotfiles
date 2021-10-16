665f882b795ab85ec057d4300e8efd031a9859b6
Javier López Úbeda <jlopezcur@gmail.com>
2021-10-17 01:06:53 +0200

    feat(sway): update password and otp scripts
    No need to -o since is copied with -c
    Also organized as otp/... for filter only otp in the command

--- sway/.config/sway/scripts/otp.sh
+++ sway/.config/sway/scripts/otp.sh
@@ -2,5 +2,5 @@
 
 # Requirements: gopass & bemenu
 
-key="$(gopass ls -f | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p 'OTP')"
-[ -n ${key} ] && gopass otp -o -c $key
+key="$(gopass ls -f | rg otp | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p 'OTP')"
+[ -n ${key} ] && gopass otp -c $key
--- sway/.config/sway/scripts/password.sh
+++ sway/.config/sway/scripts/password.sh
@@ -3,4 +3,4 @@
 # Requirements: gopass & bemenu
 
 key="$(gopass ls -f | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p 'Password')"
-[ -n ${key} ] && gopass -o -c $key
+[ -n ${key} ] && gopass -c $key
