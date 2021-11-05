05dcddfd137e655802c6e8c00e7066dfd142c666
Javier López Úbeda <jlopezcur@gmail.com>
2021-11-05 23:01:45 +0100

    feat(nvim): telescope improvements

--- nvim/.config/nvim/lua/lt/telescope/init.lua
+++ nvim/.config/nvim/lua/lt/telescope/init.lua
@@ -1,22 +1,31 @@
-local actions = require('telescope.actions')
+local actions = require("telescope.actions")
 
-require('telescope').setup {
+require("telescope").setup {
   defaults = {
-    prompt_prefix = ' ﮊ ',
+    prompt_prefix = " ﮊ ",
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<C-h>"] = "which_key"
+      }
+    }
   },
+  pickers = {
+    buffers = {
+      sort_lastused = true,
+      ignore_current_buffer = true
+    },
+    git_branches = {
+      sort_lastused = true,
+      ignore_current_buffer = true
+    }
+    -- find_files = {
+    --   theme = "dropdown"
+    -- }
+  }
 }
-require('telescope').load_extension('fzy_native')
 
-local opts = { noremap = true, silent = true }
-local map = vim.api.nvim_set_keymap
-map('n', '<leader>ff', "<cmd>lua require('lt.telescope.project_picker').project_files()<CR>", opts)
-map('n', '<leader>fg', ":Telescope live_grep<CR>", opts)
-map('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers({ sort_lastused = true, ignore_current_buffer = true })<CR>", opts)
-map('n', '<leader>fr', "<cmd>lua require('telescope.builtin').registers()<CR>", opts)
-map('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
+require("telescope").load_extension("fzy_native")
 
-map('n', 'gr', "<cmd>lua require('telescope.builtin').lsp_references()<CR>", opts)
--- Remember: Ctrl-a create a new brnach, Ctrl-d remove selected branch
-map('n', '<leader>gb', "<cmd>lua require('telescope.builtin').git_branches({ sort_lastused = true, ignore_current_buffer = true })<CR>", opts)
-map('n', '<leader>gc', "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", opts)
--- map('n', '<leader>gs', "<cmd>lua require('telescope.builtin').git_status()<CR>", opts)
+require "lt.telescope.mappings"
+
---
+++ nvim/.config/nvim/lua/lt/telescope/mappings.lua
@@ -0,0 +1,17 @@
+local opts = {noremap = true, silent = true}
+local map = vim.api.nvim_set_keymap
+
+-- Files
+map("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files{}<CR>", opts)
+map("n", "<leader>fg", "<cmd>lua require'telescope.builtin'.live_grep{}<CR>", opts)
+map("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.buffers{}<CR>", opts)
+map("n", "<leader>fh", "<cmd>lua require'telescope.builtin'.help_tags{}<CR>", opts)
+
+-- LSP
+map("n", "gr", "<cmd>lua require'telescope.builtin'.lsp_references{}<CR>", opts)
+
+-- Git
+-- Remember: Ctrl-a create a new brnach, Ctrl-d remove selected branch
+map("n", "<leader>gb", "<cmd>lua require'telescope.builtin'.git_branches{}<CR>", opts)
+map("n", "<leader>gc", "<cmd>lua require'telescope.builtin'.git_bcommits{}<CR>", opts)
+map("n", "<leader>gt", "<cmd>lua require'telescope.builtin'.git_stash{}<CR>", opts)
