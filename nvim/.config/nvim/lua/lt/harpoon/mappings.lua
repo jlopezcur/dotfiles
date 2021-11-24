059f7e3226c181b0161c897163eb6141187a54dc
Javier López Úbeda <jlopezcur@gmail.com>
2021-11-24 03:17:43 +0100

    feat(nvim): change harpoon mappings

--- nvim/.config/nvim/lua/lt/harpoon/mappings.lua
+++ nvim/.config/nvim/lua/lt/harpoon/mappings.lua
@@ -1,9 +1,9 @@
 local opts = {noremap = true, silent = true}
 local map = vim.api.nvim_set_keymap
 
-map("n", "<space>hh", "<cmd>lua require'harpoon.ui'.toggle_quick_menu()<CR>", opts)
-map("n", "<space>ha", "<cmd>lua require'harpoon.ui'.nav_file(1)<CR>", opts)
-map("n", "<space>hs", "<cmd>lua require'harpoon.ui'.nav_file(2)<CR>", opts)
-map("n", "<space>hd", "<cmd>lua require'harpoon.ui'.nav_file(3)<CR>", opts)
-map("n", "<space>hf", "<cmd>lua require'harpoon.ui'.nav_file(4)<CR>", opts)
-map("n", "<space>hm", "<cmd>lua require'harpoon.mark'.add_file()<CR>", opts)
+map("n", "<space>h", "<cmd>lua require'harpoon.ui'.toggle_quick_menu()<CR>", opts)
+map("n", "<space>j", "<cmd>lua require'harpoon.ui'.nav_file(1)<CR>", opts)
+map("n", "<space>k", "<cmd>lua require'harpoon.ui'.nav_file(2)<CR>", opts)
+map("n", "<space>l", "<cmd>lua require'harpoon.ui'.nav_file(3)<CR>", opts)
+map("n", "<space>;", "<cmd>lua require'harpoon.ui'.nav_file(4)<CR>", opts)
+map("n", "<space>m", "<cmd>lua require'harpoon.mark'.add_file()<CR>", opts)
