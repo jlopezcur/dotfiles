a04b894f699fcfa6d73e3798ac4ff0c6457324a1
Javier López Úbeda <jlopezcur@gmail.com>
2021-12-16 03:36:59 +0100

    feat(nvim): restore cmp CR for expand

--- nvim/.config/nvim/lua/lt/cmp/init.lua
+++ nvim/.config/nvim/lua/lt/cmp/init.lua
@@ -18,7 +18,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
