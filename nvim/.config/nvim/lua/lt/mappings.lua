4c0e9a5e11db8dc9157b55eab10ad08a8ad2d0b6
Javier López Úbeda <jlopezcur@gmail.com>
2021-12-18 00:38:28 +0100

    feat(nvim): change cargo run to term

--- nvim/.config/nvim/lua/lt/mappings.lua
+++ nvim/.config/nvim/lua/lt/mappings.lua
@@ -7,7 +7,7 @@ vim.api.nvim_set_keymap('n', '<Leader>qf', "<cmd>_ciwimportf=dwdwifrom lx$hxj"
 vim.api.nvim_set_keymap('n', '<Leader>lp', ":!pdflatex %<cr>", options)
 
 -- _ciwimportf=dwdwifrom lx$hxj
-vim.api.nvim_set_keymap('n', '<Leader>rr', ':TermExec cmd="cargo run" go_back=0 direction=vertical size=80<cr>', options)
+vim.api.nvim_set_keymap('n', '<Leader>rr', ':term cargo run<cr>i', options)
 
 -- close a buffer
 vim.api.nvim_set_keymap('n', '<Leader>gq', "<cmd>:BufDel<CR>", options)
