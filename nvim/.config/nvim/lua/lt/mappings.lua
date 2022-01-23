945a4a546c70b37c178ad36a4600e20dbb1b9a43
Javier López Úbeda <jlopezcur@gmail.com>
2022-01-23 04:20:30 +0100

    feat(nvim): improved shortcuts for latex

--- nvim/.config/nvim/lua/lt/mappings.lua
+++ nvim/.config/nvim/lua/lt/mappings.lua
@@ -4,7 +4,8 @@ options = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<Leader>qf', "<cmd>_ciwimportf=dwdwifrom lx$hxj", options)
 
 -- tex
-vim.api.nvim_set_keymap('n', '<Leader>lp', ":!pdflatex %<cr>", options)
+vim.api.nvim_set_keymap('n', '<Leader>lp', ":!pandoc -o '%:p:h/%:t:r.pdf' '%'<cr>", options)
+vim.api.nvim_set_keymap('n', '<Leader>lf', ":!pdflatex '%'<cr>", options)
 
 -- _ciwimportf=dwdwifrom lx$hxj
 vim.api.nvim_set_keymap('n', '<Leader>rr', ':tabnew<cr>:term cargo run<cr>i', options)
