ca8d72c5fe10dc76e6f717ed98b628851dd7f6cf
Javier López Úbeda <jlopezcur@gmail.com>
2022-01-24 08:37:13 +0100

    feat(nvim): change netrw sort

--- nvim/.config/nvim/lua/lt/netrw/init.lua
+++ nvim/.config/nvim/lua/lt/netrw/init.lua
@@ -1,7 +1,7 @@
 vim.g.netrw_bufsettings = 'noma nomod nowrap ro nobl rnu'
 vim.g.netrw_liststyle = 0
 -- vim.g.netrw_fastbrowse = 0
-vim.g.netrw_sort_by = "exten name"
+vim.g.netrw_sort_by = "name exten"
 -- vim.g.netrw_list_hide ="*facets"
 -- vim.cmd("let g:netrw_list_hide= '^\.\.\=/\=$'")
 -- vim.g.netrw_keepdir = 0
