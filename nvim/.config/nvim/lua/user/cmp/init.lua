492ef3f0500fc29bdb51f77a5e4d30202d521f91
Javier López Úbeda <jlopezcur@gmail.com>
2022-02-25 01:22:26 +0100

    feat(nvim): improve cmp

--- nvim/.config/nvim/lua/user/cmp/init.lua
+++ nvim/.config/nvim/lua/user/cmp/init.lua
@@ -1,56 +1,80 @@
 local luasnip = require("luasnip")
-local cmp = require('cmp')
-local lspkind = require('lspkind')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
+cmp.setup(
+  {
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
       end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'buffer' },
-    { name = 'spell' },
-  },
-  formatting = {
-    format = lspkind.cmp_format({with_text = true, maxwidth = 50})
-  },
-  -- experimental = {
-  --   native_menu = true
-  -- },
-  completion = {
-    completeopt = 'menu,menuone,noselect',
+    },
+    duplicates = {
+      nvim_lsp = 1,
+      luasnip = 1,
+      cmp_tabnine = 1,
+      buffer = 1,
+      path = 1
+    },
+    confirm_opts = {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false
+    },
+    documentation = {
+      -- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      border = {" ", " ", " ", " ", " ", " ", " ", " "}
+    },
+    mapping = {
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm({select = false}),
+      ["<Tab>"] = cmp.mapping(
+        function(fallback)
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end,
+        {"i", "s"}
+      ),
+      ["<S-Tab>"] = cmp.mapping(
+        function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+        {"i", "s"}
+      )
+    },
+    sources = {
+      {name = "nvim_lsp"},
+      {name = "nvim_lua"},
+      {name = "luasnip"},
+      {name = "buffer"},
+      {name = "path"},
+      {name = "spell"}
+    },
+    formatting = {
+      format = lspkind.cmp_format({with_text = false, maxwidth = 50})
+    },
+    experimental = {
+      ghost_text = false,
+      native_menu = false
+    },
+    completion = {
+      completeopt = "menu,menuone,noselect"
+    }
   }
-})
+)
