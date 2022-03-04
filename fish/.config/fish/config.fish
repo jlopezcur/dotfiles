496c27f4d7600e7c35168302faf078e27675c33f
Javier López Úbeda <jlopezcur@gmail.com>
2022-03-04 22:31:22 +0100

    feat(fish): set instead export and nvm start

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,36 +19,31 @@
 #                ///-._ _ _ _ _ _ _}^ - - - - ~                     ~-- ,.-~
 #                                                                   /.-~
 
-# Set default programs
-export EDITOR="nvim"
-export VISUAL="nvim"
-export BROWSER="qutebrowser"
-export PAGER="less"
-# export MANPAGER="nvim"
+# default applications
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx TERMINAL alacritty
+set -gx TERMCMD alacritty
+set -gx BROWSER qutebrowser
+set -gx PAGER less
+set -gx READER zathura
+# set -gx MANPAGER nvim
 
 # Eye Candy
 # https://wiki.archlinux.org/title/Color_output_in_console
-export LESS='-R --use-color -Dd+r$Du+b'
+set -gx LESS -R --use-color -Dd+r$Du+b
 
 # Set general architecture flags
-export ARCHFLAGS="-arch x86_64"
+set -gx ARCHFLAGS -arch x86_64
 
 # Source other files
-
 [ -f ~/.config/fish/aliases.fish ] && . ~/.config/fish/aliases.fish
 
 # Starship
-
 starship init fish | source
 
-# Check for a .nvmrc file and use it for change node version
-# function __check_nvmrc --on-variable PWD --description 'Do nvm stuff'
-#   status --is-command-substitution; and return
-#   if test -f .nvmrc
-#     nvm use || nvm install
-#   end
-# end
+# nvm
+source $HOME/.config/fish/nvm.fish
 
-# You must call it on initialization or listening to directory switching won't
-# work
-load_nvm
+# nnn
+source $HOME/.config/fish/nnn.fish
