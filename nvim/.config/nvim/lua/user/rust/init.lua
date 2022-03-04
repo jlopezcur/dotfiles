835a5c3f3992a9a4a12ff062ad78a18603a4643e
Javier López Úbeda <jlopezcur@gmail.com>
2022-03-04 22:53:25 +0100

    fix(nvim): rust lsp with rust-tools

--- nvim/.config/nvim/lua/user/rust/init.lua
+++ nvim/.config/nvim/lua/user/rust/init.lua
@@ -1,1 +1,8 @@
-require('rust-tools').setup({})
+require("rust-tools").setup(
+  {
+    server = {
+      cmd = {vim.fn.stdpath "data" .. "/lsp_servers/rust/rust-analyzer"},
+      standalone = false
+    }
+  }
+)
