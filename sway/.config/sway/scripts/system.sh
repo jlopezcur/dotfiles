afb3c08ff6e2012d73927cb616e8a3cf88b58f79
Javier López Úbeda <jlopezcur@gmail.com>
2022-04-21 01:37:48 +0200

    fix(sway): firm update system entry

--- sway/.config/sway/scripts/system.sh
+++ sway/.config/sway/scripts/system.sh
@@ -3,15 +3,13 @@
 # Requirements: bemenu
 
 actions=(
-  "lock"
-  "logout"
-  "hibernate"
-  "reboot"
-  "poweroff"
-  "update"
-  "check email"
-  "check updates"
-  "firmware upgrade"
+  " lock"
+  " logout"
+  " hibernate"
+  " reboot"
+  "⏻ poweroff"
+  " check email"
+  " firmware upgrade"
 )
 
 list=$(printf "\n%s" "${actions[@]}") # list of entries in lines
@@ -19,14 +17,12 @@ list="${list:1}" # remove the first separator
 action=$(echo "${list}" | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p "System")
 
 case $action in
-  "lock") swaylock ;;
-  "logout") swaymsg exit ;;
-  "hibernate") systemctl hibernate ;;
-  "reboot") systemctl reboot ;;
-  "poweroff") systemctl poweroff ;;
-  "update") alacritty -e system-update ;;
-  "check email") mbsync gmail ;;
-  "check updates") store-package-updates ;;
-  "firmware upgrade") alacritty -e "fwupdmgr refresh; fwupdmgr get-updates; fwupdmgr update" ;;
+  " lock") swaylock ;;
+  " logout") swaymsg exit ;;
+  " hibernate") systemctl hibernate ;;
+  " reboot") systemctl reboot ;;
+  "⏻ poweroff") systemctl poweroff ;;
+  " check email") mbsync gmail ;;
+  " firmware upgrade") alacritty -e firm-update ;;
   *) echo "Invalid option $action" ;;
 esac
