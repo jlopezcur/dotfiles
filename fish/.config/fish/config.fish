c392c80e037e07914723f24061adf42a0e8d0eeb
Javier López Úbeda <jlopezcur@gmail.com>
2022-06-14 04:49:23 +0200

    feat(fish): update config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,31 +19,33 @@
 #                ///-._ _ _ _ _ _ _}^ - - - - ~                     ~-- ,.-~
 #                                                                   /.-~
 
-# default applications
-set -gx EDITOR nvim
-set -gx VISUAL nvim
-set -gx TERMINAL alacritty
-set -gx TERMCMD alacritty
-set -gx BROWSER qutebrowser
-set -gx PAGER less
-set -gx READER zathura
-# set -gx MANPAGER nvim
+if status is-interactive
+  # default applications
+  set -gx EDITOR nvim
+  set -gx VISUAL nvim
+  set -gx TERMINAL alacritty
+  set -gx TERMCMD alacritty
+  set -gx BROWSER qutebrowser
+  set -gx PAGER less
+  set -gx READER zathura
+  # set -gx MANPAGER nvim
 
-# Eye Candy
-# https://wiki.archlinux.org/title/Color_output_in_console
-set -gx LESS -R --use-color -Dd+r$Du+b
+  # Eye Candy
+  # https://wiki.archlinux.org/title/Color_output_in_console
+  set -gx LESS -R --use-color -Dd+r$Du+b
 
-# Set general architecture flags
-set -gx ARCHFLAGS -arch x86_64
+  # Set general architecture flags
+  set -gx ARCHFLAGS -arch x86_64
 
-# Source other files
-[ -f ~/.config/fish/aliases.fish ] && . ~/.config/fish/aliases.fish
+  # Source other files
+  [ -f ~/.config/fish/aliases.fish ] && . ~/.config/fish/aliases.fish
 
-# Starship
-starship init fish | source
+  # Starship
+  starship init fish | source
 
-# nvm
-source $HOME/.config/fish/nvm.fish
+  # nvm
+  source $HOME/.config/fish/nvm.fish
 
-# nnn
-source $HOME/.config/fish/nnn.fish
+  # nnn
+  source $HOME/.config/fish/nnn.fish
+end
