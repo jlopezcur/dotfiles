096fd0e5fd527688c7d50eb89940f20c739723b6
Javier López Úbeda <jlopezcur@gmail.com>
2022-06-25 02:01:10 +0200

    feat(nvim): change tokyonight style

--- nvim/.config/nvim/lua/user/theme.lua
+++ nvim/.config/nvim/lua/user/theme.lua
@@ -5,7 +5,7 @@
 
 -- require('onenord').setup()
 
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent = false
 vim.cmd[[colorscheme tokyonight]]
