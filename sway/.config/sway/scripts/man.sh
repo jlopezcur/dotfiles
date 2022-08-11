93b8d3adaa32647d632a49e3f36ba004cfd0c67f
Javier López Úbeda <jlopezcur@gmail.com>
2022-08-11 03:21:09 +0200

    feat(sway): removed man command

--- sway/.config/sway/scripts/man.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Requirements: bemenu
-
-page="$(apropos . | awk '{print $1}' | bemenu -H 25 --tf '#268bd2' --hf '#268bd2' -p 'Man Page')"
-export MANPAGER="nvim -c 'set ft=man' -"
-[ -n "$page" ] && alacritty -e man $page
