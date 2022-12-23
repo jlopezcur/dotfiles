3eb92ac497bd0c4d696148c35ebe6b5b99fcbdb8
Javier López Úbeda <jlopezcur@gmail.com>
2022-12-23 18:34:14 +0100

    fix(sway): POSIX compliant some scripts

--- sway/.config/sway/scripts/system.sh
+++ sway/.config/sway/scripts/system.sh
@@ -2,18 +2,12 @@
 
 # Requirements: wofi
 
-actions=(
-  " lock"
-  " logout"
-  " hibernate"
-  " reboot"
-  "⏻ poweroff"
-  " check email"
-  " firmware upgrade"
-)
-
-list=$(printf "\n%s" "${actions[@]}") # list of entries in lines
-list="${list:1}" # remove the first separator
+list=" lock
+ logout
+ hibernate
+ reboot
+⏻ poweroff
+ firmware upgrade"
 action=$(echo "${list}" | wofi -i --show dmenu -p "System command...")
 
 case $action in
@@ -22,7 +16,6 @@ case $action in
   " hibernate") systemctl hibernate ;;
   " reboot") systemctl reboot ;;
   "⏻ poweroff") systemctl poweroff ;;
-  " check email") mbsync gmail ;;
   " firmware upgrade") alacritty -e firm-update ;;
   *) echo "Invalid option $action" ;;
 esac
--- sway/.config/sway/scripts/vpn.sh
+++ sway/.config/sway/scripts/vpn.sh
@@ -2,14 +2,9 @@
 
 # Requirements: wofi
 
-actions=(
-  "start"
-  "check"
-  "stop"
-)
-
-list=$(printf "\n%s" "${actions[@]}") # list of entries in lines
-list="${list:1}" # remove the first separator
+list="start
+check
+stop"
 action=$(echo "${list}" | wofi -i --show dmenu -p "VPN command...")
 
 case $action in
