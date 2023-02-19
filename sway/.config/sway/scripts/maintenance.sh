b50c5cb4e7e66f9a07ab37a3afc5976b77d88520
Javier López Úbeda <jlopezcur@gmail.com>
2023-02-19 23:40:44 +0100

    feat(sway): added more maintenance menu entries

--- sway/.config/sway/scripts/maintenance.sh
+++ sway/.config/sway/scripts/maintenance.sh
@@ -6,11 +6,20 @@ list="NVTOP: GPU Top
 HTOP: Interactive process viewer
 TOP: Classical process viewer
 BTOP: Tunned process viewer
+BMON: Monitoring bandwidth
 paru -Qe: list of explicit installed packages
 Upgrade firmware
 GPing google.com
 Internal IP
-External IP"
+External IP
+Update Clamav: update antivirus
+Clamscan /home/jlopez/
+rkhunter: check for rootkits
+Diskonaut: Disk space navigator
+GDU: Disk usage analyzer
+hwinfo
+iftop
+kmon: kernel monitor"
 action=$(echo "${list}" | wofi -i --show dmenu -p "Maintenance command...")
 
 case $action in
@@ -18,10 +27,19 @@ case $action in
   "HTOP: Interactive process viewer") alacritty -e htop ;;
   "TOP: Classical process viewer") alacritty -e top ;;
   "BTOP: Tunned process viewer") alacritty -e btop ;;
+  "BMON: Monitoring bandwidth") alacritty -e bmon ;;
   "paru -Qe: list of explicit installed packages") alacritty -e paru -Qe ;;
   "Upgrade firmware") alacritty -e firm-update ;;
   "GPing google.com") alacritty -e gping google.com ;;
   "Internal IP") alacritty -e ip route ;;
   "External IP") alacritty -e curl -s https://icanhazip.com ;;
+  "Update Clamav: update antivirus") alacritty -e sudo freshclam ;;
+  "Clamscan /home/jlopez/") alacritty -e clamscan --recursive --infected /home/jlopez/ ;;
+  "rkhunter: check for rootkits") alacritty -e sudo rkhunter --check --sk ;;
+  "Diskonaut: Disk space navigator") alacritty -e diskonaut ;;
+  "GDU: Disk usage analyzer") alacritty -e gdu ;;
+  "hwinfo") alacritty -e hwinfo ;;
+  "iftop") alacritty -e sudo iftop ;;
+  "kmon: kernel monitor") alacritty -e sudo kmon ;;
   *) echo "Invalid option $action" ;;
 esac
